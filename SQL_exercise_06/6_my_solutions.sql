90c3e7fcef99fb449b0b4e965f9b478204720b40
calvinkma <calvinma1999@hotmail.com>
2022-07-07 16:52:06 -0400

    Complete SQL Exercise 6

---
+++ SQL_exercise_06/6_my_solutions.sql
@@ -0,0 +1,10 @@
+-- https://en.wikibooks.org/wiki/SQL_Exercises/Scientists
+-- 6.1 List all the scientists' names, their projects' names, 
+    -- and the hours worked by that scientist on each project, 
+    -- in alphabetical order of project name, then scientist name.
+SELECT Scientists.Name, Projects.Name, Projects.Hours 
+FROM Scientists JOIN AssignedTo ON Scientists.SSN==AssignedTo.Scientist JOIN Projects ON AssignedTo.Project==Projects.Code
+ORDER BY Projects.Name ASC, Scientists.Name ASC;
+
+-- 6.2 Select the project names which are not assigned yet
+SELECT Projects.Name FROM Projects LEFT JOIN AssignedTo ON Projects.Code==AssignedTo.Project WHERE AssignedTo.Scientist IS NULL;
